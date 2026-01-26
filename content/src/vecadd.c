5e6c8e9277d58028c5961714ce1bbde1f900106d
Jonathan Appavoo <jappavoo@bu.edu>
2026-01-26 18:44:05 -0500

    vecadd pthreads

--- content/src/vecadd.c
+++ content/src/vecadd.c
@@ -1,8 +1,9 @@
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <pthread.h>
 
 #define DIM 1024
-
 long A[DIM], B[DIM], C[DIM];
 
 void
@@ -20,13 +21,46 @@ void loadVec(long *v, int dim)
   }
 }
 
+// struct to pass args to work thread
+struct wargs {
+  int dim;
+  long *a, *b, *c;
+};
+
+// entry point for additional worker thread
+//  1. unpack arguments, and
+//  2. call vecAdd
+void *worker(void *arg)
+{
+  struct wargs *wa = arg;
+  vecAdd(wa->a, wa->b, wa->c, wa->dim);
+}
+
 int main(int argc, char **argv)
 {
+  struct wargs wa;
+  pthread_t tid;
+  int rc, hd;
+
+  // load A and B vectors
   loadVec(A, DIM);
   loadVec(B, DIM);
-  bzero(C, sizeof(C));
 
-  vecAdd(A, B, C, DIM);
+  // split work in half
+  hd = DIM/2;
   
+  // pack arguments for worker and create thread
+  wa.a = A; wa.b = B; wa.c = C; wa.dim = hd;
+  rc = pthread_create(&tid, NULL, worker, &wa);
+  assert(rc == 0);
+
+  // main thread takes care of remainder
+  vecAdd(&A[hd], &B[hd], &C[hd], DIM-hd);
+
+  // wait for worker
+  rc = pthread_join(tid, NULL);
+  assert(rc == 0);
+
+  // Normally we would do something with C
   return 0;
 }
